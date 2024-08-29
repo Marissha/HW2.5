a5f44148ad3d9dabbd3e9f062f9bb957d591cc16
Marissha <balabobalaba71@gmail.com>
2024-08-29 17:29:03 +0300

    HW.py

--- HW.py
+++ HW.py
@@ -1,17 +1,14 @@
-import random
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
 
-
-def get_matrix():
-    value = [3, 2, 5, 4, 1, 6]
-    n = random.choice(value)
-    m = random.choice(value)
-    v = random.choice(value)
-    return n, m, v
-n, m, v = get_matrix()
-matrix = []
-for i in range(n):
-    matrix_1 = []
-    for j in range(m):
-        matrix_1.append(v)
-        matrix.append(matrix_1)
-    print(matrix_1)
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
